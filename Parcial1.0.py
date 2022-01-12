f7f9415c2cfd978334b3ba27825c76307194c049
Joseph Trigos <89653047+Josephts1@users.noreply.github.com>
2022-01-12 14:39:36 -0500

    Update Parcial1.0.py

--- Parcial1.0.py
+++ Parcial1.0.py
@@ -3,9 +3,9 @@ import random
 nfilas=eval(input("Digite el numero de filas de la matriz:"))
 ncolumnas=eval(input("Digite el numero de columnas de la matriz:"))
 matriz=[]
-for i in range(nfilas+1):
+for i in range(nfilas):
     filas=[]
-    for j in range(ncolumnas+1):
+    for j in range(ncolumnas):
         filas.append(random.randint(0,20))
     matriz.append(filas)
 print(matriz)
@@ -14,12 +14,12 @@ def SumaH():
     sumH=0
     max=0
     listH=[]
-    for i in range(nfilas+1):
-        for j in range(ncolumnas+1):
+    for i in range(nfilas):
+        for j in range(ncolumnas):
             sumH+=matriz[i][j]
         listH.append(sumH)
         sumH=0
-    print(listH)
+    #print(listH)
     for i in listH:
         if i>max:
             max=i
@@ -28,12 +28,12 @@ def SumV():
     sumV=0
     max=0
     listV=[]
-    for j in range(ncolumnas+1):
-        for i in range(nfilas+1):
+    for j in range(ncolumnas):
+        for i in range(nfilas):
             sumV+=matriz[i][j]
         listV.append(sumV)
         sumV=0
-    print(listV)
+    #print(listV)
     for i in listV:
         if i>max:
             max=i
@@ -43,7 +43,7 @@ def SumD():
     a=0
     max=0
     listD=[]
-    for i in range(nfilas+1):
+    for i in range(nfilas):
         for j in range(0,1):
             sumD+=matriz[i][j+a]
         a+=1
@@ -57,9 +57,9 @@ def SumAD():
     a=0
     max=0
     listAD=[]
-    for i in range(nfilas+1,0,-1):
-        for j in range(ncolumnas,ncolumnas-2,-1):
-            sumAD+=matriz[i][j+a]
+    for i in range(nfilas):
+        for j in range(ncolumnas-1,ncolumnas-2,-1):
+            sumAD+=matriz[i][j-a]
         a+=1
         listAD.append(sumAD)
     print(listAD)
@@ -67,10 +67,17 @@ def SumAD():
         if i>max:
             max=i
     return max
-maxlistH=SumaH()
-maxlistV=SumV()
-maxlistD=SumD()
-maxlistAD=SumAD()
-print(maxlistH)
-print(maxlistV)
-print(maxlistD)
+def numMaximo():
+    maxlistH=SumaH()
+    maxlistV=SumV()
+    maxlistD=SumD()
+    maxlistAD=SumAD()
+    nummaximos=[maxlistH,maxlistV,maxlistD,maxlistAD]
+    print(f"Esta es la lista con los numeros mayores de cada suma: {nummaximos}")
+    m=0
+    for i in nummaximos:
+        if i>m:
+            m=i
+    return m
+numeroMaximo=numMaximo()
+print(numeroMaximo)
