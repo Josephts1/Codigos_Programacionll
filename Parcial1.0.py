0368f176c891707cbaccd72a5afecfda461d7128
Joseph Trigos <89653047+Josephts1@users.noreply.github.com>
2022-01-11 21:01:35 -0500

    Update Parcial1.0.py

--- Parcial1.0.py
+++ Parcial1.0.py
@@ -1,14 +1,76 @@
-matriz1=[[1,2],[3,4]]
-matriz2=[[5,6],[7,8]]
-fila1=[]
-def kronecker(matriz1,matriz2):
-    espaciosvacio=[]
-    for i in range(len(matriz1)):
-        espaciosvacio.append(["$"])
-        #print(fila1)
-        for v in fila1:
-            espaciosvacio.append(v*2)
-            print(v)
-
-    print(espaciosvacio)
-kronecker(matriz1,matriz2)
+#Creacion de la matriz--------------------------------------------
+import random
+nfilas=eval(input("Digite el numero de filas de la matriz:"))
+ncolumnas=eval(input("Digite el numero de columnas de la matriz:"))
+matriz=[]
+for i in range(nfilas+1):
+    filas=[]
+    for j in range(ncolumnas+1):
+        filas.append(random.randint(0,20))
+    matriz.append(filas)
+print(matriz)
+#-----------------------------------------------------------------
+def SumaH():
+    sumH=0
+    max=0
+    listH=[]
+    for i in range(nfilas+1):
+        for j in range(ncolumnas+1):
+            sumH+=matriz[i][j]
+        listH.append(sumH)
+        sumH=0
+    print(listH)
+    for i in listH:
+        if i>max:
+            max=i
+    return max
+def SumV():
+    sumV=0
+    max=0
+    listV=[]
+    for j in range(ncolumnas+1):
+        for i in range(nfilas+1):
+            sumV+=matriz[i][j]
+        listV.append(sumV)
+        sumV=0
+    print(listV)
+    for i in listV:
+        if i>max:
+            max=i
+    return max
+def SumD():
+    sumD=0
+    a=0
+    max=0
+    listD=[]
+    for i in range(nfilas+1):
+        for j in range(0,1):
+            sumD+=matriz[i][j+a]
+        a+=1
+        listD.append(sumD)
+    for i in listD:
+        if i>max:
+            max=i
+    return max
+def SumAD():
+    sumAD=0
+    a=0
+    max=0
+    listAD=[]
+    for i in range(nfilas+1,0,-1):
+        for j in range(ncolumnas,ncolumnas-2,-1):
+            sumAD+=matriz[i][j+a]
+        a+=1
+        listAD.append(sumAD)
+    print(listAD)
+    for i in listAD:
+        if i>max:
+            max=i
+    return max
+maxlistH=SumaH()
+maxlistV=SumV()
+maxlistD=SumD()
+maxlistAD=SumAD()
+print(maxlistH)
+print(maxlistV)
+print(maxlistD)
